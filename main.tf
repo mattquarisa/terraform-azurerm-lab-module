119cbceac92ab477d8919e04fa70d8b03c45928e
Matt Quarisa <mquarisa@cisco.com>
2024-01-02 07:55:25 -0500

    Initial version:

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+    required_providers {
+        source = "hashicorp/azurerm"
+    }
+}
+
+provider "azurerm" {
+    features {}
+    skip_provider_registration = true
+}
+
+locals {
+    tags {
+        "environment" = var.environment
+    }
+}
+
+resource "azurerm_storage_account" "storage" {
+    name = var.storage_name
+    resource_group_name = var.resource_group_id
+    location = var.location
+    account_tier = "Standard"
+    public_network_access_enabled = false
+    account_replication_type = var.environment == "Production" ? "GRS" : "LRS"
+
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "resource_group_id" {
+    type = string
+} 
+
+variable "environment" {
+    type = string
+}
+
+variable "location" {
+    type = string
+}
+
+variable "storage_name" {
+    type string
+}
+
